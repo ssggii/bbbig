933afd5b6013a149463f65c5d1e65c3f392aaed3
ssggii <haan7525@gmail.com>
2026-04-13 17:53:35 +0900

    [REFACTOR] 레포지토리의 수정 메서드 삭제
    수정 메서드는 서비스 계층에서 조회와 저장 메서드를 결합하여 구현하기로 결정함에 따라 기존 수정 메서드를 삭제했습니다.

--- src/main/java/com/bbbig/repository/ReadingRecordRepository.java
+++ src/main/java/com/bbbig/repository/ReadingRecordRepository.java
@@ -11,11 +11,7 @@ public interface ReadingRecordRepository {
     List<ReadingRecord> findAll(); // 독서기록 전체 조회
 
     Optional<ReadingRecord> findById(Long id); // 독서기록 상세 조회
-
-    void updateStatus(Long id, String status); // 독서 기록의 상태 변경
-
-    void updateSentences(Long id, String sentences); // 독서 기록의 좋았던 문장 변경
-
+    
     void deleteById(Long id); // 독서 기록 삭제
 
     boolean existsById(Long id); // 독서기록 존재 여부 검사

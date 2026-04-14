c810db1e1738554700129e47544a46fec8a5e907
ho <parkhb1181@gmail.com>
2026-04-14 19:26:23 +0900

    [FEAT] 리뷰 CRUD C 작성 및 예외 처리

--- src/main/java/com/bbbig/model/Review.java
+++ src/main/java/com/bbbig/model/Review.java
@@ -11,21 +11,27 @@ public class Review {
     private LocalDateTime updatedAt;
 
     public Review(Long bookId, Double rating, String content) {
-        if (rating == null || rating < 1.0 || rating > 5.0) {
-            throw new IllegalArgumentException("평점은 1~5점 사이여야 합니다");
-        }
-        if (content == null || content.trim().isEmpty()) {
-            throw new IllegalArgumentException("리뷰 내용 비울수 없습니다");
-        }
+        ratingCook(rating);
+        contentCook(content);
 
-        if (content.length() > 50) {
-            throw new IllegalArgumentException("리뷰 내용은 50자를 초과할 수 없습니다");
-        }
         this.bookId = bookId;
         this.rating = rating;
         this.content = content;
         this.createdAt = LocalDateTime.now();
         this.updatedAt = this.createdAt;
+    }
+
+    
+    private void ratingCook(Double rating) {
+        if (rating == null || rating < 1.0 || rating > 5.0) {
+            throw new IllegalArgumentException("평점은 1~5점 사이여야 합니다");
+        }
+    }
+
+    private void contentCook(String content) {
+        if (content == null || content.trim().isEmpty() || content.length() > 50) {
+            throw new IllegalArgumentException("리뷰 내용은 비우거나 50글자를 초과해서 안됩니다");
+        }
     }
 
 
---
+++ src/main/java/com/bbbig/model/ReviewService.java
@@ -0,0 +1,22 @@
+package com.bbbig.model;
+
+import com.bbbig.repository.ReviewRepository;
+
+public class ReviewService {
+    private final ReviewRepository repository;
+
+
+    public ReviewService(ReviewRepository repository) {
+        this.repository = repository;
+    }
+
+    public void createReview(Long bookId, double rating, String content) {
+
+        Review newReview = new Review(bookId, rating, content);
+        repository.save(newReview);
+
+
+
+    }
+
+}
